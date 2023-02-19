8b1c7c70d8850cf7e2d323eb40d752900f96a70f
Nirali Dhorajiya <106247518+nirali957@users.noreply.github.com>
2023-02-19 11:09:33 +0530

    odd

---
+++ odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,n=1;
+    for(a=1;a<=5;a++){
+        for(b=1;b<=5;b++){
+            printf("\t %d",n);
+        }
+        printf("\n");
+        n+=2;
+    }
+    return 0;
+}
