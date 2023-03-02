626ce83741c47b81ff1a4fc19b2a6c2b453240bb
Nirali Dhorajiya <106247518+nirali957@users.noreply.github.com>
2023-03-02 19:02:53 +0530

    o

--- 0101_pattern.c
+++ 0101_pattern.c
@@ -25,3 +25,21 @@ int main() {
 010
 1010
 10101
+
+   
+   
+   #include <stdio.h>
+
+int main() {
+   int i,j,k=1;
+   for(i=1;i<=5;i++){
+      
+       for(j=1;j<=i;j++){
+           printf("%d ",k++%2);
+       }
+      
+       printf("\n");
+   }
+   return 0;
+  
+}
