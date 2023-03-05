3730b6ef54a223fd48aa776de63433320fa33e43
Nirali Dhorajiya <106247518+nirali957@users.noreply.github.com>
2023-03-05 12:17:52 +0530

    n

--- pyramid.c
+++ pyramid.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int i,j,sp=10;
+  for(i=1;i<=5;i++){
+      for(j=1;j<=sp;j++)
+          printf(" ");
+    for(j=1;j<=i;j++){
+        printf("* ");
+    }
+    printf("\n");
+    sp--;
+    }
+  return 0;
+}
+
+
+
+
+          * 
+         * * 
+        * * * 
+       * * * * 
+      * * * * * 
+
+            
+            
 #include <stdio.h>  
   
 int main()  
